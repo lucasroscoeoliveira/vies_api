de38531b3ba1e1e5f005f2f80ef118188c1c0c14
lucasrloliveira <lucasrloliveira@gmail.com>
2021-03-11 23:10:36 -0300

    adjust migrations to init development

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,9 +14,10 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->id();
+            $table->string('email')->nullable(false);
+            $table->string('token')->nullable(false);
+            $table->timestamps();
         });
     }
 
